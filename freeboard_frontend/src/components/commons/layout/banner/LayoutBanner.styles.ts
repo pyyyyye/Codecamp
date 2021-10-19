7ec92154acd56d1d89c6928ad471df4d849ad053
pyyyyye <qkzud8033@hanmail.net>
2021-10-19 22:18:51 +0900

    1019

--- freeboard_frontend/src/components/commons/layout/banner/LayoutBanner.styles.ts
+++ freeboard_frontend/src/components/commons/layout/banner/LayoutBanner.styles.ts
@@ -3,7 +3,7 @@ import styled from '@emotion/styled'
 export const BannerWrap = styled.div`
   width: 100%;
   height: 700px;
-  background-color: #777;
+  background-color: #888;
   font-size: 5px;
   z-index: 500;
   color: #fff;
